4f6edd005e3c37afc7c9f4de7f9aff90d1a16b36
ertivardisferivardi <levanjmurgvashvili@gmail.com>
2024-03-25 22:47:42 +0400

    commit

---
+++ day 06/classwork/lesson06.py
@@ -0,0 +1,16 @@
+         #creating inputs
+#name = input("Please enter your name: ")
+#lastname = input("Please enter your lastname: ")
+#age = input("Please enter your age: ")
+
+#print("Email inputs are: ",name,lastname,age)
+
+num1 = int(input("please enter your number: "))
+num2 = int(input("please enter your number: "))
+num3 = int(input("please enter your number: "))
+print(num1 + num2 + num3)
+print(num1 * num2 + num3)
+
+
+
+
