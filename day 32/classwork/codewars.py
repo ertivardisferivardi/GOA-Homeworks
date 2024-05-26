9d7959d9a106f95d2a5c8925213dba6afdc50bf0
ertivardisferivardi <levanjmurgvashvili@gmail.com>
2024-05-26 22:58:26 +0400

    second commit

---
+++ day 32/classwork/codewars.py
@@ -0,0 +1,51 @@
+# def update_light(current):
+#     if current == "green":
+#         return "yellow"
+#     elif current == "yellow":
+#         return "red"
+#     return "green" 
+
+
+
+# def find_difference(a, b):
+#     a_multiply = a[0] * a[1] * a[2]
+#     b_multiply = b[0] * b[1] * b[2]
+#     return abs(a_multiply - b_multiply)
+# print(find_difference([2,3,4],[3,4,5] ))
+
+
+# def arithmetic(a, b, operator):
+#     assembly = a + b
+#     subtract = a - b
+#     multiply = a * b
+#     divided = a / b
+    
+#     if operator == "add":
+#         return assembly
+#     elif operator == "subtract":
+#         return subtract
+#     elif operator == "multiply":
+#         return multiply
+#     return divided
+
+
+# def in_asc_order(arr):
+#     return arr == sorted(arr)
+
+
+
+# def show_sequence(n):
+#     if n < 0:
+#         return str(n) + "<0"
+#     elif n == 0:
+#         return "0=0"
+#     else:
+#         sum = 0
+#         list = []
+#         for i in range(n+1):
+#             sum += i
+#             list.append(str(i))
+#         return  "+".join(list) + " = " + str(sum)
+
+
+
